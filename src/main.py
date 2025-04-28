a3fdbe324ba7197647720b5b8cd7948f25d33a43
Ryan Puglia <e12341328@student.tuwien.ac.at>
2025-04-28 18:43:42 +0200

    Added classes for each type of lambda expression, as well as an abstract parent class for general expressions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,29 @@
 import sys
 
+# There are 3 lambda expressions which can make up a term: A varaibale,
+# a lambda abstraction and an application
+
+# LambdaExpression serves as an abstract
+
+
+class LambdaExpression:
+    """
+    Abstract base class for each valid Lambda Expression
+    """
+    pass
+
+
+class Var(LambdaExpression):
+    pass
+
+
+class Abs(LambdaExpression):
+    pass
+
+
+class App(LambdaExpression):
+    pass
+
 
 def main():
     file = open(sys.argv[1]).read()
