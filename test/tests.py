e40acf1966fab9a7ef405306c43e66f0c35d4a75
Ryan Puglia <e12341328@student.tuwien.ac.at>
2025-04-29 09:44:23 +0200

    Added unit tests for the functioanlities which are to be implemented. These tests will likely be tweaked/changed a bit later

--- test/tests.py
+++ test/tests.py
@@ -1,4 +1,3 @@
-import lambdaInterpreter
 import unittest
 import sys
 import os
@@ -6,29 +5,43 @@ import os
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..")))
 
+from lambdaInterpreter import Var, Abs, App, free_variables, substitute, alpha_conversion, beta_reduction, normalise, pretty_print, Parser
 
 class TestLambdaInterpreter(unittest.TestCase):
     def test_free_variables(self):
-        pass
+        self.assertEqual(free_variables(Var('x')), {'x'})
 
     def test_substitute(self):
-        pass
+        # (x y)[x := (λz.z) y] => ((λz.z) y)
+        expr = App('x', App(Var('x'), Var('y')))
+        repl = Abs('z', Var('z'))
+        result = substitute(expr, 'x', repl)
+        self.assertEqual(result.arg, Var('y'))
 
     def test_alpha_conversion(self):
-        pass
+        orig = Abs('x', App(Var('x'), Var('y')))
+        renamed = alpha_conversion(orig, 'w')
+        self.assertEqual(free_variables(renamed), {'y'})
 
     def test_beta_reduction(self):
-        pass
+        # (λx.x) y => y
+        expr = App(Abs('x', Var('x')), Var('y'))
+        reduced = beta_reduction(expr)
+        self.assertEqual(reduced, Var('y'))
 
     def test_normalise(self):
-        pass
+        expr = App(Abs('x', Var('x')), Var('y'))
+        nf = normalise(expr)
+        self.assertEqual(pretty_print(nf), 'y')
 
     def test_pretty_print(self):
-        pass
+        expr = Abs('x', Var('x'))
+        self.assertEqual(pretty_print(expr), '(λx.x)')
 
     def test_parser(self):
-        pass
+        p = Parser('x')
+        self.assertEqual(p.parse_expr(), Var('x'))
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     unittest.main()
